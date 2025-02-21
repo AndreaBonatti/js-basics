c822d19c2b2c1600ff0790b480aa6ba54c3591e5
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-21 10:40:02 +0100

    primitive types

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const interestRate = 0.3;
-// interestRate = 1; constant cannot be re-assingned
-console.log(interestRate);
+let name ='Mosh'; // String literal
+let age = 30; // Number literal
+let isApproved = true // Boolean literal
+let firstName; // Undefined
+let lastName = null; // Used to clear the value of a variable
