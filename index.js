a82fdf96f9335774cf059590b9f694a6234f4cf5
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-21 10:32:39 +0100

    variables example

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// This is my first javascript code!
-console.log("Hello World");
+let name = "Mosh";
+console.log(name);
+
+// Naming variables rules:
+// Cannot be a reserved keyword
+// Should be meaningfull
+// Cannot start with a number
+// Connot contain a space or a hypen (-)
+// Use camelCase
+// Names are case-sensitive
+
+// let firstName = "Mosh", lastName = "Hamedani";
+// Modern best practice is to declare each variable in a single line
+let firstName = "Mosh";
+let lastName = "Hamedani";
