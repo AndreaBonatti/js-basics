9cd8e799258165061fbe00ed14d7e01fd3d6065e
Andrea Bonatti <andreabonatti92@gmail.com>
2025-02-21 10:58:11 +0100

    objects

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-let name ='Mosh'; // String literal
-let age = 30; // Number literal
-let isApproved = true // Boolean literal
-let firstName; // Undefined
-let lastName = null; // Used to clear the value of a variable
+let person = {
+  name: "Mosh",
+  age: 30,
+}; // {} Object literal, this is an object with two properties
+
+console.log(person);
+
+// Dot notation
+person.name = "John";
+
+console.log(person.name);
+
+// Bracket notation
+person["name"];
+
+console.log(person["name"]);
+
+// Dot notation is more compact and concise => default choice
+// If we have the parameter selection at run-time => is better to use the bracket notation
+// e.g. let selection = 'name' => console.log(person[selection])
